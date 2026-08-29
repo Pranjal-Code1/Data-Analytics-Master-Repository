28ba5a182228bc46051d13e2c3e66e2d3277279f
Pranjal <pranjaltanwar655@gmail.com>
2026-08-29 10:35:07 +0530

    feat(tuples): add Python data structures tuple practice and notes

---
+++ 01-Python-Powerhouse/Python/Class 12/class12.py
@@ -0,0 +1,46 @@
+#tuple 
+
+# a = (12,12.4,"hell",print())
+#tuple has hetrogenous nature
+
+#tuple has a immutable nature 
+# a = (1,2,3,4,5)
+
+# a[1] = 20 #there will be an error 
+
+#Tuple can store duoplicate values 
+# a = (1,2,3,4,1,1,1,2,2,2,3,3)
+
+#tuple unpacking 
+
+# a,b,c = (10,20,30)
+# print(b)
+
+# a = (1,)
+
+# print(type(a))
+
+# a = (10,20,30,40,50)
+
+# print(a[:3])
+
+# a = [10,20,30,40]
+
+# tup = tuple(a)
+
+# print(tup)
+
+
+# t = (10,20,30,40,50)
+
+#1st way 
+# for i in t:
+#     print(i)
+
+#2nd way 
+# for i in range(len(t)):
+#     print(t[i])
+
+# a = (10,20,30,40,40,40,50)
+# print(a.count(40))
+# print(a.index(30))
