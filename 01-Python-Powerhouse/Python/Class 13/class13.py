91f28bfa16348a5070ecddc5a0fc5ea475b6d2d4
Pranjal <pranjaltanwar655@gmail.com>
2026-08-30 19:07:32 +0530

    feat(sets): add Python data structures set practice and notes

---
+++ 01-Python-Powerhouse/Python/Class 13/class13.py
@@ -0,0 +1,27 @@
+# s = {10,"hello",(1,2,3),12.5,True,print()}
+# #you can only store hashable values inside set
+
+# #unordered nature
+# s = {10,40,50,20,60,80}
+# print(s[0])
+
+#you cannot have duplicate values 
+# s = {10,10,10,20,20,20,30,30,30}
+# print(s)
+
+# a = [12,12,12,34,34,6,7,7,8,0,7,4,3,2]
+# s = set(a)
+# print(s)
+
+
+#traversing on the set 
+
+# s = {12,34,56,12,12,78,23}
+
+# for i in s:
+#     print(i)
+
+# s1 = {10,20,30,40}
+# s2 = {30,40,50,60} 
+
+# print(s1 | s2)
