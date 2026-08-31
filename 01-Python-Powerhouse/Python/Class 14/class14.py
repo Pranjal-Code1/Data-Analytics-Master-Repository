502871cc3204525e1670781e9ce08283c3d66339
Pranjal <pranjaltanwar655@gmail.com>
2026-08-31 21:50:37 +0530

    feat(class14): add notes and code examples for Data Structure Dictionary

---
+++ 01-Python-Powerhouse/Python/Class 14/class14.py
@@ -0,0 +1,35 @@
+# d = {"hello":100,"bro":200,"sunday":300}
+
+
+# print(d["hello"]) #accessing the values 
+
+
+# d['hello'] = 1000 #I am changing the values 
+
+# d = {10:100,20:200,30:300,40:400}
+
+# print(d[10])
+
+# d = dict(name = "akarsh",age = 24, gender = "Male")
+
+# print(d)
+
+# d = dict([("name","Akarsh"),("age",24)])
+
+# print(d)
+
+#Dictionary Trasversing
+
+# a = {10:100,20:200,30:300,40:400}
+
+# for i in a.values():
+#     print(i)
+
+# help(dict)
+
+# a = {10:100,20:200,30:300}
+# b = {30:3000,50:500,60:600}
+
+# a.update(b)
+
+# print(a)
