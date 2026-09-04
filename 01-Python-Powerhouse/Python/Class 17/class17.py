b2b290729eeca0101f603dc67dcfa3195c9d5d70
Pranjal <pranjaltanwar655@gmail.com>
2026-09-04 17:33:24 +0530

    Add Class 17: File handling implementation

---
+++ 01-Python-Powerhouse/Python/Class 17/class17.py
@@ -0,0 +1,15 @@
+# file = open(r"Class 6/class6.py")
+
+# print(file.read())
+
+# open("pull.txt",'x')
+
+# file = open("push.txt",'a')
+
+# file.write("hello this is a sample file that I have created ")
+
+# file.close()
+
+
+# with open("class16.py",'r') as fs:
+#     print(fs.read())
