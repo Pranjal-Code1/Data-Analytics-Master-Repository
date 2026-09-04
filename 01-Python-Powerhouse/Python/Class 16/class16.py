9ea5baeacbf52e7da90d82430eaa1715e83a26e5
Pranjal <pranjaltanwar655@gmail.com>
2026-09-04 15:05:46 +0530

    Add Class 16: Exception handling implementation

---
+++ 01-Python-Powerhouse/Python/Class 16/class16.py
@@ -0,0 +1,26 @@
+# a = int(input("provide your numbers : -"))
+# b = int(input("provide your numbers : -"))
+
+# try:
+#     print(a/b)
+
+# except ZeroDivisionError as err:
+#     print(f"sorry an error occured as {err}")
+
+# else:
+#     print("there was no erros ")
+
+# finally:
+#     print("I will execute no matter what !!")
+
+# print(a+b)
+
+
+
+# try:
+#     age = int(input("Enter age: "))
+#     if age < 18:
+#         raise Exception("You must be 18+")
+#     print("Access granted")
+# except Exception as e:
+#     print("Error:", e)
