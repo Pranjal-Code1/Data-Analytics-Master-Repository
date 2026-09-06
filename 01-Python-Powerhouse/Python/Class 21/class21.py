0646944eeae16d884edd790c6492162b0cb80220
Pranjal <pranjaltanwar655@gmail.com>
2026-09-07 00:48:21 +0530

    feat(class21): implement constructors in Python OOP

---
+++ 01-Python-Powerhouse/Python/Class 21/class21.py
@@ -0,0 +1,21 @@
+# def factory(material,zips,pockets):
+#     pass
+
+# factory()
+
+# class Factory:
+#     def __init__(self,material,zips,pockets):
+#         # print(self)
+#         self.material = material 
+#         self.zips = zips 
+#         self.pockets = pockets
+    
+#     def showdetails(self):
+#         print(self.material,self.pockets,self.zips)
+
+
+
+# reebok = Factory("Leather",3,3)
+# campus = Factory("nylon",2,2)
+
+# campus.showdetails()
