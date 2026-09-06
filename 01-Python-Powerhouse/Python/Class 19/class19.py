bb3cba9b9fc90e80d6c98377848c2a8ed2ab6d09
Pranjal <pranjaltanwar655@gmail.com>
2026-09-06 11:33:32 +0530

    Add Class 19: Classes in OOPs

---
+++ 01-Python-Powerhouse/Python/Class 19/class19.py
@@ -0,0 +1,9 @@
+# class Factory:
+#     a = 12  #A variable stored inside a class is Attribute.
+#     def hello():   #A function stored inside a class is Method.
+#         print("how are you")
+
+#     print("I am getting initialized")
+
+# print(Factory.a)
+# Factory.hello()
