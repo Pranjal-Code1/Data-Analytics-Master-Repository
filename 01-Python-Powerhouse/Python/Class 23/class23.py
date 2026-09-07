35b4fe06d170d85d03a818fc34dd32da8af57bd7
Pranjal <pranjaltanwar655@gmail.com>
2026-09-07 16:07:56 +0530

    feat(class23): implement inheritance and parent-child class concepts in Python

---
+++ 01-Python-Powerhouse/Python/Class 23/class23.py
@@ -0,0 +1,48 @@
+# class Animal:    #parent class, super class 
+#     def __init__(self,name,age):
+#         self.name = name
+#         self.age = age 
+    
+#     def info(self):
+#         print(f"your name is {self.name} and your age is {self.age}")
+
+
+
+# class Human(Animal):  #child class, sub class
+#     def __init__(self, name, age , number, group):
+#         super().__init__(name, age)
+#         self.number = number
+#         self.group = group 
+
+
+# class Robots(Human):
+#     def __init__(self, name, age, number, group,imei):
+#         super().__init__(name, age, number, group)
+#         self.imei = imei
+
+
+
+# obj = Animal("lion",12)
+# obj2 = Human("Akarsh",24,1234567890,"B+")
+
+# obj2.info()
+
+
+# class Animal:
+#     name = "Lion"
+
+# class Human:
+#     name = "Vansh"
+
+# class Robots(Human,Animal):
+#     pass
+
+
+# class Animal:
+#     pass 
+
+# class Human(Animal):
+#     pass 
+
+# class Robots(Animal):
+#     pass 
