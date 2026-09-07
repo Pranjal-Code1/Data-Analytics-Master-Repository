c03990a929bf0df646eea7806276fe44f750b71b
Pranjal <pranjaltanwar655@gmail.com>
2026-09-07 16:10:42 +0530

    feat(class24): implement encapsulation and data protection in Python OOP

---
+++ 01-Python-Powerhouse/Python/Class 24/class24.py
@@ -0,0 +1,14 @@
+# class Animal:
+#     __name = "lion"
+
+#     def speak(self):
+#         print("hello I will roar")
+
+
+# class Human(Animal):
+#     def say(self):
+#         print(f"hello my name is {super().__name} ")
+
+# obj = Human()
+
+# obj.say()
