a3bee5f30579a7e6a2552175caa849e0f13676ab
Pranjal <pranjaltanwar655@gmail.com>
2026-09-08 07:18:58 +0530

    feat(class26): implement abstraction and hidden logic in Python OOP

---
+++ 01-Python-Powerhouse/Python/Class 26/class26.py
@@ -0,0 +1,20 @@
+# from abc import ABC, abstractmethod
+
+# class Animal(ABC):
+#     @abstractmethod
+#     def sound(self):
+#         pass 
+   
+
+
+# class Dog(Animal):
+#     def sound(self):
+#         print("Hello I make woff sound!")
+#     def hello(self):
+#         print("I am a dog and I woof")
+
+# class cat(Animal):
+#     def sound(self):
+#         print("hello I make meow sound")
+
+# obj = Dog()
