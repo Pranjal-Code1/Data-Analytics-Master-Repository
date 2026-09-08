95b9ce6f6f748872d3be6047a8e12d43f60c49d0
Pranjal <pranjaltanwar655@gmail.com>
2026-09-08 07:01:41 +0530

    feat(class25): implement polymorphism and real-life examples in Python OOP

---
+++ 01-Python-Powerhouse/Python/Class 25/class25.py
@@ -0,0 +1,31 @@
+# class Animal:
+#     name = "lion"
+#     def speak(self):
+#         print("hello I roar")
+
+# class Bird:
+#     name = "sparrow"
+
+#     def speak(self):
+#         print("hello i tweet")
+
+# obj = Animal()
+# obj2 = Bird()
+
+# obj.speak()
+# obj2.speak()
+
+# class Animal:
+#     name = "lion"
+#     def speak(self):
+#         print("hello I roar")
+
+# class Human(Animal):
+#     name = "Akarsh"
+
+#     def speak(self):
+#         super().speak()
+#         print("hello my name is akarsh")
+
+# obj = Human()
+# obj.speak()
