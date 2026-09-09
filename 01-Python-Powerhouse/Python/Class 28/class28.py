318318ac7c1dd42ccb0b6330990f355777df771d
Pranjal <pranjaltanwar655@gmail.com>
2026-09-09 23:42:53 +0530

    Add Class 28: Modules and Packages notes and code

---
+++ 01-Python-Powerhouse/Python/Class 28/class28.py
@@ -0,0 +1,17 @@
+# import math_utils
+# from math_utils import add ,sub
+# from math_utils import * 
+# print(add(12,12))
+
+# # buildin modules
+# import math 
+# import random 
+# import string 
+
+# # A module is a simple python file (.py)
+# # it contaisn code - Functions, Classes , Variables that you can resuse 
+
+# from my_package import math_utils, string_util
+
+# from my_package.math_utils import add
+# from my_package.string_util import to_upper
